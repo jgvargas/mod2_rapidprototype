7884a97d8ea6be2c998517bedfd266e20e6ac942
jgvargas <juan.vargas01@utrgv.edu>
2017-04-02 13:07:14 -0500

    Add files via upload
    Apr 2nd scripts

---
+++ PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	// Physics component
+	public Rigidbody rigidbody_ref;
+
+	void Start()
+	{
+		rigidbody_ref = GetComponent<Rigidbody> ();
+	}
+
+	//Update: Called before a frame is rendered. 
+
+	//FixedUpdate: Before any physics is applied
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Ribidbody: class, .AddForce to move RB
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody_ref.AddForce (movement * speed);
+	}
+}
