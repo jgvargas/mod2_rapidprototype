1b6cc962c8a1cfe078a27e6d4bbbd50e4a1ad3c9
SaulPerales <saul_perales@hotmail.com>
2017-04-04 12:31:53 -0500

    Added my rapid prototype assets

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour {
+
+    public float respawnTime = 10f;
+    public float nextRespawn = 0f;
+    GameObject []test;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (gameObject.activeSelf)
+
+            print("Updating");
+        if (Time.time > nextRespawn + respawnTime)
+        {
+            print(10);
+            nextRespawn = Time.time;
+            gameObject.SetActive(true);
+        }
+
+
+    }
+}
